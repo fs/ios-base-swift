30a66e8c3e6684b1a1afd783b1036181dc5b82db
VladimirGoncharov <vladimir.goncharov@flatstack.com>
2017-03-25 03:11:13 +0400

    Remove and update old data

--- generate_entities.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PROJECT_NAME = "Swift-Base"
-PATH = "$PWD/$PROJECT_NAME/CoreData"
-
-mogenerator --swift --model "$PATH/$PROJECT_NAME.xcdatamodeld" --machine-dir "$PATH/Private/" --human-dir "$PATH"
-
-sleep 3
