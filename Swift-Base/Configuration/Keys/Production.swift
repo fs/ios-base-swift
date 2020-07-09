f8a8fb9a933ceed613340ce0a68982567cbbe05d
Timur <33258940+TimurShafigullin@users.noreply.github.com>
2020-07-09 12:17:25 +0300

    Remove unused schemes. Create Staging target. (#83)

---
+++ Swift-Base/Configuration/Keys/Production.swift
@@ -0,0 +1,16 @@
+//
+//  Production.swift
+//  Swift-Base
+//
+//  Created by Timur Shafigullin on 09/07/2020.
+//  Copyright © 2020 Flatstack. All rights reserved.
+//
+
+import Foundation
+
+enum Keys {
+
+    // MARK: - Type Properties
+
+    static let apiServerBaseURL = URL(string: "https://Swift-Base.com")!
+}
